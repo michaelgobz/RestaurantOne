98ff1674a5a415b41ba979bb76f89e4d60b1fc48
Jed-hub <jedidiabahena@gmail.com>
2023-03-11 23:13:43 +0000

    added tablename

--- models/reservations/reservation.py
+++ models/reservations/reservation.py
@@ -1,26 +1,22 @@
 from app import db
 
 class Reservation(db.Model):
-    """
-    Initialize a Reservation instance
-    """
+ #   Representation of a reservation
+
+    __tablename__ = 'reservations'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    description = db.Column(db.Text, nullable=False)
-    duration = db.Column(db.DateTime, nullable=False)
-    start = db.Column(db.DateTime, nullable=False)
-    end = db.Column(db.DateTime, nullable=False)
-    nb_of_person = db.Column(db.Integer, nullable=False)
-    additional_info = db.Column(db.Text)
-    tables = db.Column(db.Integer)
-    category = db.Column(db.String(50))
+    description = db.Column(db.Text, nullable=True)
+    duration = db.Column(db.DateTime, nullable=True)
+    start = db.Column(db.DateTime, nullable=True)
+    end = db.Column(db.DateTime, nullable=True)
+    nb_of_person = db.Column(db.Integer, nullable=False, default=0)
+    additional_info = db.Column(db.Text, nullable=True)
+    tables = db.Column(db.Integer, nullable=True)
+    category = db.Column(db.String(50), nullable=True)
+    price = db.Column(db.Integer, nullable=False, default=0)
+    tax = db.Column(db.Integer, nullable=True)
+    menu_item = db.Column(db.String(50), nullable=True)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    tax = db.Column(db.Integer, nullable=False)
-    menu_item = db.Column(db.String(50))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __init__(self):
-        """ Initialize Reservation instance
-        """
-        pass
