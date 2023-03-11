d3076d140125e43a3fb5cfe0aca2962e8f9be5d0
Jed-hub <jedidiabahena@gmail.com>
2023-03-11 23:22:01 +0000

    added elements in the model

--- models/menus/menu_item.py
+++ models/menus/menu_item.py
@@ -1,15 +1,16 @@
 from app import db
 
 class MenuItem(db.Model):
-    """
-    Creating a Menu model
-    """
+#   Representation of a menu item
+
+    __tablename__ = 'menu_items'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    category = db.Column(db.String(50))
-    price = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(50), nullable=True)
+    price = db.Column(db.Integer, nullable=False, default=0)
     foods = db.Column(db.String(50), nullable=False)
-    toppings = db.Column(db.String(50), nullable=False)
-    serving_model = db.Column(db.String(50), nullable=False)
-    tax_associated = db.Column(db.Integer, nullable=False)
+    toppings = db.Column(db.String(50), nullable=True)
+    serving_model = db.Column(db.String(50), nullable=True)
+    tax_associated = db.Column(db.Integer, nullable=True)
