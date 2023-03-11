63dc7f205c41f48d17e057fbc3fb0a6f2f235d03
Jed-hub <jedidiabahena@gmail.com>
2023-03-11 23:18:22 +0000

    added table element

--- models/products/product.py
+++ models/products/product.py
@@ -1,20 +1,21 @@
 from app import db
 
 class Product(db.Model):
-    """
-    Initialize a Menu instance
-    """
+# Representation of a product
+
+    __tablename__ = 'products'
+
     id = db.Column(db.Integer, primary_key=True)
     short_name = db.Column(db.String(50), nullable=False)
-    long_name = db.Column(db.String(50), nullable=False)
+    long_name = db.Column(db.String(50), nullable=True)
     description = db.Column(db.Text, nullable=False)
-    menu_category = db.Column(db.String(50))
-    menu_item = db.Column(db.String(50))
-    owner = db.Column(db.String(50))
-    price = db.Column(db.Integer, nullable=False)
-    varients = db.Column(db.String(50))
+    menu_category = db.Column(db.String(50), nullable=True)
+    menu_item = db.Column(db.String(50), nullable=True)
+    owner = db.Column(db.String(50), nullable=True)
+    price = db.Column(db.Integer, nullable=False, default=0)
+    varients = db.Column(db.String(50), nullable=True)
     is_available = db.Column(db.Boolean, nullable=False)
     is_deliverable = db.Column(db.Boolean, nullable=False)
     duration_of_preparation = db.Column(db.DateTime)
-    max_quantity = db.Column(db.Integer)
-    # location = db.Column(db.String(50))
+    max_quantity = db.Column(db.Integer, nullable=False, default=0)
+    location = db.Column(db.String(50), nullable=True)
