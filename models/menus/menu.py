b176bf2c56cff41e8c27714cef7e6a354f5ab7e3
Jed-hub <jedidiabahena@gmail.com>
2023-03-11 23:23:12 +0000

    added elements in the model

--- models/menus/menu.py
+++ models/menus/menu.py
@@ -1,13 +1,14 @@
 from app import db
 
 class Menu(db.Model):
-    """
-    Initialize a Menu instance
-    """
+#    Representation of a menu
+
+    __tablename__ = 'menus'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
     description = db.Column(db.Text, nullable=False)
     category = db.Column(db.String(50))
     items = db.Column(db.String(50))
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
-    owner_restaurant_name = db.Column(db.String(50))
+    owner_restaurant_name = db.Column(db.String(50), nullable=True)
