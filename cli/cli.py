b741ddb78f5eeb4d15aa59051bfaeaebaf7626fe
michaelgobz <raniahgobz@outlook.com>
2023-03-11 11:32:15 +0000

    cli is working

--- cli/cli.py
+++ cli/cli.py
@@ -1,14 +1,7 @@
 """CLI module."""
-import sys
 from model import CLI
 
 cli = CLI()
 
 if __name__ == '__main__':
-    if(len(sys.argv) == 1):
-        cli.cli()
-    elif(sys.argv[1] == 'migrate'):
-        cli.migrate()
-    elif(sys.argv[1] == 'loadData'):
-        cli.load_data()
-    
+    cli.cli()  
--- cli/model.py
+++ cli/model.py
@@ -7,37 +7,27 @@ class CLI:
     def __init__(self):
         self._db_client = ""
         self._random_data = dict()
-          
-    @click.command()
+        self._migrate_tasks = dict()
+        
+    @click.group()
     def cli():
         """Welcome to the RestaurantOne Management System CLI.
-        commands:
-                migrate - Migrate data into the database
-                loadData - Load basic data into the database
-                help - Show this help message
-        Options: 
-                None:
-        Messages:
-                Migration completed successfully
-                Data loaded successfully
-        Error Codes: 
-                0 - No error
-                1 - Error
         """
         click.echo('Welcome to the RestaurantOne Management System CLI.')
         
     @click.command()
-    def migrate(self):
-        """Migrate command"""
+    def migrate():
+        """migrate models to the database"""
         click.echo('Migrating data into the database')
         # call the migrate function from the respective module
         click.echo('Migration completed successfully')
 
     @click.command()
-    def load_data(self):
-        """Load data command"""
+    def load_data():
+        """load data to the database"""
         click.echo('Loading data into the database')
         # call the load_data function from the respective module
         click.echo('Data loaded successfully')
         
-    
+    cli.add_command(migrate)
+    cli.add_command(load_data)
