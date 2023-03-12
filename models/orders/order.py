890cbb4049bab3089f8c77a3a27f15b578c36055
Jed-hub <jedidiabahena@gmail.com>
2023-03-12 13:16:40 +0000

    updated order module

--- models/orders/order.py
+++ models/orders/order.py
@@ -1,15 +1,18 @@
-from app import db
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
 
-class Order(db.Model):
+Base = declarative_base()
+
+class Order(Base):
 #    Representation of an order
 
     __tablename__ = 'orders'
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    items = db.Column(db.String(50), nullable=False)
-    total_cart_price = db.Column(db.Integer, nullable=False, default=0)
-    address = db.Column(db.Text, nullable=False)
-    shipment_method = db.Column(db.String(50), nullable=False)
-    payment_method = db.Column(db.String(50), nullable=False)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    items = Column(String(50), nullable=False)
+    total_cart_price = Column(Integer, nullable=False, default=0)
+    address = Column(Text, nullable=False)
+    shipment_method = Column(String(50), nullable=False)
+    payment_method = Column(String(50), nullable=False)
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'), nullable=False)
