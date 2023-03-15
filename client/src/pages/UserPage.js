8ca6bc2d546290262cfe95ce02c95626495a035e
michaelgobz <raniahgobz@outlook.com>
2023-03-15 10:39:38 +0300

    user page update

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -149,7 +149,7 @@ export default function UserPage() {
   return (
     <>
       <Helmet>
-        <title> User | Minimal UI </title>
+        <title> Reservation | Open Restaurant </title>
       </Helmet>
 
       <Container>
