a0c8f7e9935121729e3ebd120fab93b7c3f00706
Jed-hub <jedidiabahena@gmail.com>
2023-03-17 15:44:41 +0000

    imported route

--- app.py
+++ app.py
@@ -2,18 +2,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from api.routes import views
-
 # Initializing flask app
 app = Flask(__name__)
 app.secret_key = '8445a9af69bccd13de1a10de1de88158'
 
 # Initializing database with flask app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/restaurantOne'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 db = SQLAlchemy(app)
 
 # Route registration
-app.register_blueprint(views)
+from api.routes import *
 
 # Running app
 if __name__ == '__main__':
