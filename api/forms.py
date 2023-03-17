0ef8e0e540c04af46985d25bae21a4fb2d48a19a
Jed-hub <jedidiabahena@gmail.com>
2023-03-17 00:13:15 +0000

    forms for testing

--- api/forms.py
+++ api/forms.py
@@ -2,14 +2,14 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email
 
-class SignupForm():
+class SignupForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign up')
 
-class LoginForm():
+class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign up')
