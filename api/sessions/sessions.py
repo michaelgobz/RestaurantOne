dfc85444ff5f624ddf8134a90d68a4a51c16dda5
Jed-hub <jedidiabahena@gmail.com>
2023-03-17 01:26:39 +0000

    comment

--- api/sessions/sessions.py
+++ api/sessions/sessions.py
@@ -7,16 +7,16 @@ from api.core.base import Db as db
 
 
 def _generate_uuid() -> str:
-    """Returns a string representation of a new UUID
-    """
+    # Returns a string representation of a new UUID
+    
     UUID = uuid4()
     return str(UUID)
 
 class Session:
     # Manage session
     def create_session(self, email: str) -> Union[str, None]:
-        """ Returns a session id
-        """
+        #  Returns a session id
+        
         try:
             user = User.query.filter_by(email=email)
             if user is None:
@@ -34,8 +34,8 @@ class Session:
         return session_id
 
     def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
-        """Returns string argument of the corresponding user or None
-        """
+        # Returns string argument of the corresponding user or None
+        
         if session_id is None:
             return None
 
@@ -49,8 +49,8 @@ class Session:
         return user
 
     def destroy_session(self, user_id: int) -> None:
-        """ Updates the user's session ID to None
-        """
+        #  Updates the user's session ID to None
+        
         try:
             user = User.query.filter_by(id=user_id)
             if user is None:
