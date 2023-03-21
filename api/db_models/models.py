815024df2bb9ce7f00e8eb1dfd2ad36e9c18d20a
michaelgobz <raniahgobz@outlook.com>
2023-03-21 14:24:56 +0000

    migrations

--- api/db_models/models.py
+++ api/db_models/models.py
@@ -1,14 +1,11 @@
 """data models that the api uses"""
 from datetime import datetime
-from email.policy import default
-from typing import List
-from datetime import datetime
 from api.core.base import declarative_base as db
 
 
 class User(db.Model):
     """Users database model"""
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False)
@@ -21,8 +18,8 @@ class User(db.Model):
     updated_at = db.Column(db.DateTime, nullable=False,
                            default=datetime.utcnow,
                            onupdate=datetime.utcnow)
-    orders = db.relationship("Order", backref="user")
-    reservations = db.relationship('Reservation', backref='user')
+    orders = db.relationship("Order", backref="users")
+    reservations = db.relationship('Reservation', backref='users')
     reservation_id = db.Column(db.Integer, db.ForeignKey('reservations.id'))
 
     # Define many-to-many relationship with Address model
@@ -55,13 +52,13 @@ class UserAddress(db.Model):
     __tablename__ = 'users_address'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    address_id = db.Column(db.Integer, db.ForeignKey('address.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    address_id = db.Column(db.Integer, db.ForeignKey('addresses.id'))
 
 
 class Restaurant(db.Model):
     """Restaurants database model"""
-    __tablename__ = 'restaurant'
+    __tablename__ = 'restaurants'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
@@ -81,16 +78,16 @@ class Restaurant(db.Model):
                            default=datetime.utcnow,
                            onupdate=datetime.utcnow)
     menus = db.relationship("Menu",
-                            backref="restaurant",
+                            backref="restaurants",
                             cascade="all, delete-orphan")
-    shipments = db.relationship('Shipment', backref='restaurant')
-    orders = db.relationship('Order', backref='restaurant')
-    reservations = db.relationship('Reservation', backref='restaurant')
+    shipments = db.relationship('Shipment', backref='restaurants')
+    orders = db.relationship('Order', backref='restaurants')
+    reservations = db.relationship('Reservation', backref='restaurants')
 
 
 class Menu(db.Model):
     """menus database model"""
-    __tablename__ = 'menu'
+    __tablename__ = 'menus'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
@@ -109,7 +106,7 @@ class Menu(db.Model):
 
 class MenuItem(db.Model):
     """menu items database model"""
-    __tablename__ = 'menu_item'
+    __tablename__ = 'menu_items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
@@ -125,12 +122,12 @@ class MenuItem(db.Model):
     updated_at = db.Column(db.DateTime, nullable=False,
                            default=datetime.utcnow,
                            onupdate=datetime.utcnow)
-    menu_id = db.Column(db.Integer, db.ForeignKey('menu.id'), nullable=False)
+    menu_id = db.Column(db.Integer, db.ForeignKey('menus.id'), nullable=False)
 
 
 class Order(db.Model):
     """orders database model"""
-    __tablename__ = 'order'
+    __tablename__ = 'orders'
 
     id = db.Column(db.Integer, primary_key=True)
     menu = db.Column(db.String(500), nullable=False)
@@ -144,18 +141,19 @@ class Order(db.Model):
     updated_at = db.Column(db.DateTime,
                            default=datetime.utcnow,
                            onupdate=datetime.utcnow)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 
 class OrderItem(db.Model):
     """OrderItem model"""
+    __tablename__ = 'order_items'
     id = db.Column(db.Integer, primary_key=True)
 
 
 class Reservation(db.Model):
     """Reservations database model"""
-    __tablename__ = 'reservation'
+    __tablename__ = 'reservations'
 
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=True)
@@ -180,37 +178,33 @@ class Reservation(db.Model):
                         db.ForeignKey('users.id'), nullable=False)
 
 
-
 class ReservationItem(db.Model):
     """Reservation Item model"""
+    __tablename__ = 'reservation_items'
     id = db.Column(db.Integer, primary_key=True)
 
 
 class PaymentMethod(db.Model):
     """PaymentMethod model"""
+    __tablename__ = 'payment_methods'
     id = db.Column(db.Integer, primary_key=True)
 
 
 class Payment(db.Model):
     """Payment model"""
+    __tablename__ = 'payments'
     id = db.Column(db.Integer, primary_key=True)
 
 
 class TransactionItem(db.Model):
     """TransactionItem model"""
+    __tablename__ = 'transaction_items'
     id = db.Column(db.Integer, primary_key=True)
 
 
 class Transaction(db.Model):
     """Transaction model"""
-    id = db.Column(db.Integer, primary_key=True)
-    
-class ReservationItem(db.Model):
-    """Reservation Item model"""
-    id = db.Column(db.Integer, primary_key=True)
-    
-class Reservation(db.Model):
-    """Reservation model"""
+    __tablename__ = 'transactions'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=True)
     duration = db.Column(db.DateTime, nullable=True)
@@ -235,46 +229,55 @@ class Reservation(db.Model):
 
 class ShipmentMethod(db.Model):
     """Shipment method model"""
+    __tablename__ = 'shipment_methods'
     id = db.Column(db.Integer, primary_key=True)
 
 
 class Shipment(db.Model):
     """Shipment model"""
+    __tablename__ = 'shipments'
     id = db.Column(db.Integer, primary_key=True)
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
 
 
 class Invoice(db.Model):
     """ Invoice model"""
+    __tablename__ = 'invoices'
     id = db.Column(db.Integer, primary_key=True)
 
 
 class InvoiceItem(db.Model):
     """InvoiceItem model"""
+    __tablename__ = 'invoice_items'
     id = db.Column(db.Integer, primary_key=True)
 
 
 class Information(db.Model):
     """information model"""
+    __tablename__ = 'information'
     id = db.Column(db.Integer, primary_key=True)
 
 
 class Review(db.Model):
     """Review model"""
+    __tablename__ = 'reviews'
     id = db.Column(db.Integer, primary_key=True)
 
 
 class ReviewItem(db.Model):
     """ReviewItem model"""
+    __tablename__ = 'review_items'
     id = db.Column(db.Integer, primary_key=True)
 
 
 class Event(db.Model):
     """Event model"""
+    __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
 
 
 class EventItem(db.Model):
     """EventItem model"""
+    __tablename__ = 'event_items'
     id = db.Column(db.Integer, primary_key=True)
     event = db.Column(db.String(50), default="", nullable=True)
