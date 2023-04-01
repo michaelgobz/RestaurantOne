34a83180e79c1a5235208fc900cedd33170a912e
michaelgobz <raniahgobz@outlook.com>
2023-04-01 14:02:56 +0000

    changed the import case mis match

--- client/src/pages/ProductsPage.js
+++ client/src/pages/ProductsPage.js
@@ -2,7 +2,7 @@ import { Helmet } from 'react-helmet-async';
 import { useEffect, useState } from 'react';
 // @mui
 import { Container, Stack, Typography } from '@mui/material';
-import LoadingSpinner from '../sections/loadingSpinner/LoadingSpinner';
+import LoadingSpinner from '../sections/LoadingSpinner/LoadingSpinner';
 // components
 import { ProductSort, ProductList, ProductFilterSidebar } from '../sections/products';
 
