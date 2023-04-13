71e1062b5a17860f2e7fe54c9f73fe95d268481b
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2023-04-13 13:56:28 +0000

    imports setteled

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,13 +1,30 @@
 
-from .models import (Address, Cart, CartItem, Event, EventItem, Information,
-                     Invoice, InvoiceItem, Menu, MenuItem, Order, OrderItem,
-                     Payment, PaymentMethod, Reservation,
-                     Restaurant, Shipment, ShipmentMethod, Transaction,
-                     User, VerificationToken)
+from addresses.model import Address, UserAddress
+from cart.model import Cart, CartItem
+from events.model import Event, EventItem
+from invoices.model import Invoice, InvoiceItem
+from menus.model import Menu, MenuItem
+from orders.model import Order, OrderItem
+from payments.model import Payment, PaymentMethod, Transaction
+from reservations.model import Reservation
+from restaurant.model import Restaurant
+from reviews.model import ReviewItem, ReservationReview, MenuItemReview
+from shipment.model import Shipment, ShipmentMethod
+from tokens.model import VerificationToken
+from user.model import User
 
-__all__ = ['User', 'Address', 'MenuItem', 'Menu', 'OrderItem', 'Order', 'PaymentMethod',
+__all__ = ['User', 'Address',
+           'UserAddress', 'MenuItem',
+           'Menu', 'OrderItem',
+           'Order', 'PaymentMethod',
            'Payment', 'Transaction',
-           'Reservation', 'Restaurant', 'ShipmentMethod', 'Shipment', 'Invoice',
-
-           'InvoiceItem', 'Event', 'EventItem', 'Information',
-           'Cart', 'CartItem', 'VerificationToken']
+           'Reservation', 'Restaurant',
+           'ShipmentMethod', 'Shipment',
+           'Invoice', 'InvoiceItem',
+           'Event', 'EventItem',
+           'InvoiceItem', 'Event',
+           'EventItem', 'Information',
+           'ReviewItem', 'ReservationReview',
+           'MenuItemReview',
+           'Cart', 'CartItem', 'VerificationToken'
+           ]
