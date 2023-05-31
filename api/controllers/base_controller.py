2c1ab4fd705eceb86b396efe1a2b7de3134ddc3e
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2023-05-31 15:41:32 +0000

    refactoring the code to adopt a more oop style

---
+++ api/controllers/base_controller.py
@@ -0,0 +1,37 @@
+""" The base controller for all controllers in the routes. """
+
+import typing as Typing
+import requests
+from ...api.db import db
+from flask import Blueprint, abort, jsonify, redirect, request, url_for
+from flask_jwt_extended import (create_access_token, get_jwt, get_jwt_identity,
+                                )
+
+
+class BaseController:
+    """ The base controller for all controllers in the routes. """
+    _request: Typing.Optional[requests.Request] = None
+    _db_client: Typing.Optional[db] = None
+    _access_token: Typing.Optional[str] = None
+
+    def __init__(self, request, db):
+        """ The constructor for the base controller. """
+        self.request = request
+        self.db_client = db
+
+    def get_request(self) -> requests.Request:
+        """ The get method for the base controller. """
+        return self.request
+
+    def get_access_token(self) -> str:
+        """ The get method for the base controller. """
+        token = get_jwt()['jti']
+        self.access_token = token
+        return self.access_token
+
+    def get_db_client(self) -> db:
+        """ The get method for the base controller. """
+        return self.db_client
+
+
+api_controller = BaseController(request, db)
