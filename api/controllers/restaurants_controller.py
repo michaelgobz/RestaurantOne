320f4ded9f440c988739fa8ceb9c4f5c8d6ce278
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2023-07-09 14:23:31 +0000

    developing the tenant solution

--- api/controllers/restaurants_controller.py
+++ api/controllers/restaurants_controller.py
@@ -1,5 +1,6 @@
 """ restaurants_controller"""
-
+import uuid
+import datetime
 from .base_controller import BaseController
 from ..models import Restaurant
 
@@ -32,3 +33,44 @@ class RestaurantsController:
             .filter_by(id=restaurant_id)
             .first()
         )
+    
+    def get_restaurant_by_name(self, name):
+        """Get a restaurant by name"""
+        return (
+            self.get_controller()
+            .get_db_client()
+            .get_session()
+            .query(Restaurant)
+            .filter_by(name=name)
+            .first()
+        )
+        
+    def get_restaurant_by_location(self, location):
+        """Get a restaurant by location"""
+        return (
+            self.get_controller()
+            .get_db_client()
+            .get_session()
+            .query(Restaurant)
+            .filter_by(location=location)
+            .first()
+        )
+        
+    def create_restaurant(self, data):
+        """Create a restaurant"""
+        restaurant = Restaurant(
+            id = uuid.uuid4(),
+            name = data["name"],
+            description = data["description"],
+            location = data["location"],
+            is_operational = data["is_operational"],
+            order_fulfilling = data["order_fulfilling"],
+            avatar = data["avatar"],
+            created_at = datetime.datetime.utcnow(),
+            updated_at = datetime.datetime.utcnow(),
+            manager_id = data["manager_id"],
+            
+        )
+        self.get_controller().get_db_client().get_session().add(restaurant)
+        self.get_controller().get_db_client().get_session().commit()
+        return restaurant
