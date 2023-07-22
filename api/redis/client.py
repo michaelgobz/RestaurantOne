7439106aa4927e09bf5387f458a98ebccf0ad5fb
Michael Goboola <47690301+michaelgobz@users.noreply.github.com>
2023-07-22 09:41:57 +0000

    redis utlity methd

--- api/redis/client.py
+++ api/redis/client.py
@@ -21,4 +21,35 @@ class RedisClient():
         """ return the cached pay load"""
         pass
     
+    def update_payload(self, key, info):
+        """ update the cached payload"""
+        pass
+    
+    def delete_topic(self, key):
+        """ delete the topic"""
+        pass
+    
+    def get_topics(self):
+        """ get all topics"""
+        pass
+    def get_topic(self, key):
+        """ get a topic"""
+        pass
+    
+    def get_topic_info(self, key):
+        """ get a topic info"""
+        pass
+    
+    def get_topic_payload(self, key):
+        """ get a topic payload"""
+        pass
+    
+    def get_topic_payload_info(self, key):
+        """ get a topic payload info"""
+        pass
+    
+    def get_redis_info(self):
+        """ get redis info"""
+        pass
+    
         
